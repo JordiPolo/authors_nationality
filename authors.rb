7d59c6a5285ba8ecd61c828e2e7a171c190e8661
Jordi Carres <mumismo@gmail.com>
2013-08-11 02:00:32 +0900

    delete unused code and reorg of the classes

--- authors.rb
+++ authors.rb
@@ -1,13 +1,141 @@
 # -*- coding: UTF-8 -*-
 require 'net/http'
+require 'net/https'
+require 'cgi'
+
 require 'json'
 
 BOOKS_FILE = 'goodreads_export.csv'
-BIRTHPLACE_FILE = 'birthplaces.json'
+BIRTHPLACE_FILE = 'known_authors.json'
+
+class AuthorList
+  def initialize(data_array = [])
+    @list = data_array.map{|author| Author.new(author)}
+  end
+
+  def add(new_author)
+    existing_author = @list.select{|author| author.name == new_author.name }.first
+    if existing_author
+      existing_author.merge(new_author)
+    else
+      @list.push(new_author)
+    end
+    self
+  end
+
+  def serialize
+    @list.map(&:serialize).to_json
+  end
+
+  def names
+    @list.map(&:name)
+  end
+
+  def filter(known_authors)
+    return authors unless known_authors
+    @list.select{|author| !known_authors.names.include?(author.name)}
+  end
+
+  def printout
+    puts 'Authors:'
+    @list.each do |author|
+      puts "#{author.name} -> #{author.nationality}, #{author.book_count}"
+    end
+    puts 'By country:'
+    puts by_country
+    puts 'Summary:'
+    puts print_stats
+  end
+
+  def print_stats
+    puts "Country\t authors"
+    print_sorted(count_authors_by_country)
+    puts "Country\t books"
+    print_sorted(count_books_by_country)
+  end
+
+  def print_totals
+    book_count = @list.inject(0){|acc, author| acc+= author.book_count}
+    puts "Total books #{book_count}"
+    puts "Total authors #{@list.size}"
+  end
+
+private
+  def names_by_country
+    country_data = by_country
+    country_data.each_pair{|country, authors| country_data[country] = authors.map(&:name)}
+  end
+  def count_authors_by_country
+    country_data = by_country
+    country_data.each_pair{|country, authors| country_data[country] = authors.size}
+  end
+
+  def count_books_by_country
+    country_data = by_country
+    country_data.each_pair{|country, authors| country_data[country] = authors.map(&:book_count).inject(0,:+)}
+ #   by_country.map{|country, authors| {country => }
+  end
+
+  def print_sorted(data)
+    sorted = data.sort_by {|_key, value| value}.reverse
+    sorted.each do |pair|
+     puts "#{pair.first}\t#{pair.last}"
+    end
+  end
+
+  def by_country
+    @list.inject({}) do |acc, author|
+      acc[author.normalized_nationality] ||= []
+      acc[author.normalized_nationality].push author
+      acc
+    end
+  end
+end
+
+class Author
+  attr_accessor :name, :books, :nationality
+  def initialize(properties)
+    raise 'the author needs a name' unless properties['name']
+    @name = properties['name']
+    @books = properties['books'] || []
+    @nationality = properties['nationality']
+  end
+  def merge(new_author)
+    @books.push(new_author.books).flatten
+    @nationality ||= new_author.nationality
+  end
+  def book_count
+    @books.size
+  end
+
+  def serialize
+    {name: @name, nationality: @nationality, books: @books}
+  end
+
+  def normalized_nationality
+    case @nationality
+    when 'United States of America'
+      'US'
+    when 'United Kingdom'
+      'England'
+    when 'Scotland'
+      'England'
+    when 'Wales'
+      'England'
+    when 'Kingdom of Prussia'
+      'Germany'
+    else
+      @nationality
+    end
+  end
+
+end
+
+
 
 #This class manages the exported file from GoodReads.
-class GoodReadsExport
-  def self.get_authors
+class GoodReadsData
+  def self.import
     contents = File.read(BOOKS_FILE)
 
     all_books = contents.split("\n")
@@ -15,50 +143,23 @@ class GoodReadsExport
     # get rid of the headers
     books.shift
 
-    # split by "  because every field text is surrounded by quotes, it is better than commas
-    # because we are having commas everywhere
-    authors = books.map{|book| book.split("\"")[3] }
-  end
-end
 
-
-# This class connects to DBPedia to get birhplaces of authors, data is incomplete and
-# I am not using this now
-class DBPediaClient
-
-  def get_birthplace(original_author_name)
-    author_name = url_friendly_name(original_author_name)
-    response = Net::HTTP.get_response("dbpedia.org","/data/#{author_name}.json")
-    json = JSON.parse(response.body)
-    author = json && json["http://dbpedia.org/resource/#{author_name}"]
-    return nil if !author
-    birth = author && author["http://dbpedia.org/property/placeOfBirth"]
-    unless (birth && birth[0]["value"])
-      redirect = author["http://dbpedia.org/ontology/wikiPageRedirects"]
-      if redirect
-        new_name = redirect[0]['value'].split('/').last
-        puts "redirected to #{new_name}"
-        get_birthplace(new_name)
-      end
+    books.inject(AuthorList.new) do |list, book|
+      # split by "  because every field text is surrounded by quotes, it is better than commas
+      # because we are having commas everywhere
+      author = book.split("\"")[3]
+      book_title = book.split("\"")[1]
+      list.add(Author.new('name' => author, 'books' => [book_title]))
+      list
     end
-    birth && birth[0]["value"]
-  rescue Timeout::Error => e
-    puts 'TIMED OUT'
-    # if we get a timeerror just ignore the data.
   end
-
-private
-  require 'cgi'
-  def url_friendly_name(author_name)
-    CGI.escape(author_name.gsub(' ','_'))
-  end
-
 end
 
 
 # This class manages a cache file with all the results we had till now. This let us print what
 # we want or search for new authors without needing to search for all the previous authors
 class KnownAuthors
+  attr_reader :list
 
   def initialize
     contents = if File.exists?(BIRTHPLACE_FILE)
@@ -66,109 +167,23 @@ class KnownAuthors
     else
       "[]"
     end
-    @authors = JSON.parse(contents)
+    authors = JSON.parse(contents)
+    @list = authors.inject(AuthorList.new){|list, author| list.add(Author.new(author))}
+  rescue
+    @list = AuthorList.new
   end
 
-  def filter(authors)
-    return authors unless @authors
-    known_authors = @authors.map{|data| data['author']}
-    authors.select{|author| !known_authors.include?(author)}
-  end
-
-  def add(author, birthplace)
-    @authors.push({author:author, birthplace: birthplace})
+  def add(author)
+    @list.add(author)
   end
 
   def save
     File.open(BIRTHPLACE_FILE,"w") do |f|
-      f.write(@authors.uniq.to_json)
-    end
-  end
-
-  def printout
-    puts 'Authors:'
-    @authors.each do |author|
-      puts "#{author['author']} -> #{author['birthplace']}"
-    end
-    puts 'By country:'
-    puts by_country
-    puts 'Summary:'
-    puts stats
-  end
-
-  def stats
-     @authors.inject(Hash.new(0)) do |acc, author|
-      acc[author['birthplace']] +=1
-      acc
-    end
-  end
-
-  def by_country
-    contents = @authors.inject({}) do |acc, author|
-      acc[author['birthplace']] ||= []
-      acc[author['birthplace']].push author['author']
-      acc
+      f.write(@list.serialize)
     end
   end
 
 end
-
-# gem install goodreads
-require 'goodreads'
-# This class connects to Goodreads and finds the information about the nationality
-# of the author
-class GoodReadsClient
-  def initialize
-    @client = Goodreads.new(:api_key => ENV['API_KEY'])
-  end
-
-  def get_nationality(original_author_name)
-    author_data = client.author(client.author_by_name(original_author_name)['id'])
-    calculate_birthplace(author_data['about'])
-    sleep(1.1)
-  rescue Goodreads::NotFound
-    puts "#{original_author_name} not found"
-    'Unknown'
-  end
-
-  private
-
-  def nationalities
-    @nationalities ||= {
-      'French' => 'France',
-      'English' => 'England',
-      'Chinese' => 'China',
-      'American' => 'USA',
-      'Czech Republic' => 'Czech Republic',
-      'United States of America' => 'USA',
-      'Greek' => 'Greece',
-      'Italian' => 'Italy',
-      'Irish' => 'Ireland',
-      'Spanish' => 'Spain',
-      'Castilian' => 'Spain',
-      'England' => 'England',
-      'German' => 'Germany',
-      'Wisconsin' => 'USA',
-      'America' => 'USA',
-      'Japanese' => 'Japan',
-      'British' => 'England',
-      'Austrian' => 'Austria',
-      'Victorian' => 'England',
-      'Utah' => 'USA'
-    }
-  end
-
-  def calculate_birthplace(biography)
-    nationalities.each_pair do |nationality, nation|
-      if biography && biography.match(/.*#{nationality}.*/)
-        return nation
-      end
-    end
-   'Unknown'
- end
-end
-
-
 
 
 class FreeBaseClient
@@ -181,7 +196,7 @@ class FreeBaseClient
        http.get uri.request_uri
     end
     json = JSON.parse(response.body.to_s)
-    sleep 0.3
+    sleep 0.2
     if json["result"] && !json["result"].empty?
       nationalities = json["result"][0]["/people/person/nationality"]
       index = nationalities.size == 1 ? 0 : 1
@@ -191,24 +206,27 @@ class FreeBaseClient
     end
     "Unknown"
   end
+
 end
 
-authors = GoodReadsExport.get_authors
-unique_authors = authors.uniq
-puts "Total books #{authors.size}"
-puts "Total authors #{unique_authors.size}"
+
+authors = GoodReadsData.import
+authors.print_totals
 
 known_authors = KnownAuthors.new
 
-unknown_authors = known_authors.filter(unique_authors)
+unknown_authors = authors.filter(known_authors.list)
 
+#unknown_authors = known_authors.filter(authors)
+known_authors.list.printout
+raise 'end'
 #goodreads = GoodReadsClient.new
 
-unknown_authors.each do |author_name|
+unknown_authors.each do |author|
 #  nationality = goodreads.get_nationality(original_author_name)
- nationality = FreeBaseClient.get_nationality(author_name)
-  puts "#{author_name}  -> #{nationality}"
-  known_authors.add(author_name, nationality)
+  author.nationality = FreeBaseClient.get_nationality(author.name)
+  puts "#{author.name}  -> #{author.nationality}"
+  known_authors.add(author)
   known_authors.save
 end
 
